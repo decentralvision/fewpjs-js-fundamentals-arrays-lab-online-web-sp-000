d54ab91863b3d3276b4b6576a92dd4ec10b694fc
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-10-20 20:58:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const prepend = ["Milo", "Otis", "Garfield"]
 const removeLast = ["Milo", "Otis", "Garfield"]
 const removeFirst = ["Milo", "Otis", "Garfield"]
 
-function append(item){
-  append.push(item)
-}
+append.push("Odie");
+prepend.unshift("Odie");
+removeLast.pop();
+removeFirst.shift();
